2495eaee6772ae1d994df1504db39634109153df
jakegornall <jakegornall@yahoo.com>
2017-02-23 18:12:33 -0500

    chore: Add Data Structure Class
    I created class for working with BST's, Linked Lists, and Graphs.
    BST classes are found in the module "BST.py", Graph classes in
    "Graph.py", Linked List Classes in "LL.py"

--- solutions.py
+++ solutions.py
@@ -1,3 +1,7 @@
+from Graph import *
+from BST import *
+from LL import *
+
 def question1(s, t):
     '''
     Given two strings s and t, determines whether some anagram of t is a
@@ -70,68 +74,6 @@ def question3(G):
     =======
     Adjaceny List
     '''
-    pass
-
-class bstNode(object):
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-
-class BST(object):
-    def __init__(self, root, nodeList=None):
-        self.root = bstNode(root)
-        if nodeList:
-            self.build_tree(nodeList)
-
-    def build_tree(self, nodeList):
-        self.build_tree_helper(self.root, nodeList)
-
-    def build_tree_helper(self, node, nodeList):
-        for idx, link in enumerate(nodeList[node.value]):
-            if link == 1:
-                if idx < node.value:
-                    node.left = bstNode(idx)
-                    self.build_tree_helper(node.left, nodeList)
-                elif idx > node.value:
-                    node.right = bstNode(idx)
-                    self.build_tree_helper(node.right, nodeList)
-                else:
-                    raise ValueError("Invalid BST Structure For Node " + str(idx))
-
-    def ancesstor_list(self, searchValue):
-        ancesstors = []
-        while True:
-            temp = self.ancesstor(searchValue)
-            if temp:
-                ancesstors.append(temp.value)
-                if temp.value == self.root.value:
-                    return ancesstors
-                else:
-                    searchValue = temp.value
-            else:
-                return ancesstors
-                
-
-    def ancesstor(self, searchValue):
-        return self.recursive_ancess_search(self.root, searchValue)
-
-    def recursive_ancess_search(self, node, searchValue):
-        if node:
-            if node.left and searchValue == node.left.value:
-                return node
-            if node.right and searchValue == node.right.value:
-                return node
-            if searchValue < node.value:
-                return self.recursive_ancess_search(node.left, searchValue)
-            elif searchValue > node.value:
-                return self.recursive_ancess_search(node.right, searchValue)
-            else:
-                raise ValueError("An Error Occurred: Invalid Tree Structure Found.")
-        else:
-            return None
-
 
 
 def question4(T, r, n1, n2):
@@ -164,15 +106,6 @@ def question4(T, r, n1, n2):
             return n2Ancesstors[i]
         i += 1
     return None
-
-
-
-
-
-class Node(object):
-  def __init__(self, data):
-    self.data = data
-    self.next = None
 
 
 def question5(ll, m):
